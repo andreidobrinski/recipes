fd0c4d0058bfc59ef639ce277e3bb6af959d774c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2020-06-23 21:55:34 -0400

    fix gql query

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,7 @@ exports.createPages = async ({ graphql, actions }) => {
         allMdx(
           ${
             process.env.NODE_ENV === 'production'
-              ? 'filter: { frontmatter: { draft: { eq: false } } }'
+              ? 'filter: { frontmatter: { draft: { ne: true } } }'
               : ''
           }
           sort: { fields: [frontmatter___date], order: DESC }
